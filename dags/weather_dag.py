750a3666d283ed7b9f0b92db48de5e6c8fc8f8a8
Victoraraujo965 <estudoaraujo6@gmail.com>
2026-07-13 17:56:59 +0000

    feat: pipeline completa - dbt Silver e Gold integrados no Airflow (deu trabalho haha)

--- dags/weather_dag.py
+++ dags/weather_dag.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
 from datetime import datetime
 from extractors.weather_extractor import run
 
@@ -9,8 +10,20 @@ with DAG(
     schedule_interval="0 0 * * *",
     catchup=False
 ) as dag:
+
     extract_task = PythonOperator(
         task_id="weather_extract",
         python_callable=run
     )
-    extract_task
+
+    dbt_silver_task = BashOperator(
+        task_id="dbt_silver",
+        bash_command="cd /opt/airflow/weather_transform && /home/airflow/.local/bin/dbt run --select stg_weather"
+    )
+    
+    dbt_gold_task = BashOperator(
+        task_id="dbt_gold",
+        bash_command="cd /opt/airflow/weather_transform && /home/airflow/.local/bin/dbt run --select agg_weather_daily"
+    )
+
+    extract_task >> dbt_silver_task >> dbt_gold_task
